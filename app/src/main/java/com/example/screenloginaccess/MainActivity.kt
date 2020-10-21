3aaf21bddad2336238bae3751d4efa0e83024bce
Alisson D R Santos <alissondrs1>
2020-10-20 23:34:55 -0300

    Add interação para o carregamento da segunda tela do app

--- app/src/main/java/com/example/screenloginaccess/MainActivity.kt
+++ app/src/main/java/com/example/screenloginaccess/MainActivity.kt
@@ -7,18 +7,25 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    val EXTRA_MESSAGE = "EXTRA_MESSAGE"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
     fun acessApp(view: View){
-        val editTextTextPersonName3 = findViewById<EditText>(R.id.editTextTextPersonName3)
-        val message = editTextTextPersonName3.text.toString()
-        val intent = Intent(this, Display_Data_Activity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
+
+        val editTextNameUser = findViewById<EditText>(R.id.editTextTextPersonName3)
+        val userLogin = editTextNameUser.text.toString()
+
+        val editTextPwd=findViewById<EditText>(R.id.editTextTextPersonName4)
+        val userPwd = editTextPwd.text.toString()
+
+        if ((userLogin == userExample.name) and (userPwd == userExample.email) or (userLogin == userExample.CPF)){
+            val intent = Intent(this, Display_Data_Activity::class.java)
+            startActivity(intent)
         }
-        startActivity(intent)
+
 
     }
 
